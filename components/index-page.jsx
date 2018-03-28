d82ffb6a58155e628acd1117293bed780b5f5497
Hristo Spasov <hristo_sp99@abv.bg>
2018-03-28 22:28:31 +0300

    Start working on question visualisation

--- components/index-page.jsx
+++ components/index-page.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import { Link, withRouter } from "react-router-dom";
 
 class IndexPage extends React.Component {
     render() {
-        return <p>IndexPage</p>;
+        return <div>
+            <p>IndexPage</p>
+            <Link to="/question">Question</Link>
+        </div>;
     }
 }
 
-export default IndexPage;
+export default withRouter(IndexPage);
--- components/question-page.jsx
+++ components/question-page.jsx
@@ -1,8 +1,60 @@
 import React from "react";
+import {
+    Divider,
+    FlatButton,
+    RadioButtonGroup,
+    RadioButton,
+    Stepper,
+    Step,
+    StepLabel,
+    Toolbar,
+    ToolbarGroup,
+} from "material-ui";
+import NavigationExpandMoreIcon from "material-ui/svg-icons/navigation/expand-more";
+import ArrowBack from "material-ui/svg-icons/navigation/arrow-back";
+import ArrowForward from "material-ui/svg-icons/navigation/arrow-forward";
 
 class QuestionPage extends React.Component {
     render() {
-        return <p>QuestionPage</p>;
+        return <div style={{ "textAlign": "center" }}>
+            <Stepper activeStep={2}>
+                <Step><StepLabel></StepLabel></Step>
+                <Step><StepLabel></StepLabel></Step>
+                <Step><StepLabel></StepLabel></Step>
+                <Step><StepLabel></StepLabel></Step>
+                <Step><StepLabel></StepLabel></Step>
+                <Step><StepLabel></StepLabel></Step>
+                <Step><StepLabel></StepLabel></Step>
+                <Step><StepLabel></StepLabel></Step>
+                <Step><StepLabel></StepLabel></Step>
+                <Step><StepLabel></StepLabel></Step>
+            </Stepper>
+            <Toolbar>
+                <ToolbarGroup firstChild>
+                    <FlatButton
+                        onClick={() => { }}
+                        target="_blank"
+                        labelPosition="after"
+                        label="Previous"
+                        icon={<ArrowBack />}
+                    />
+                </ToolbarGroup>
+                <ToolbarGroup>
+                    <FlatButton label="Finish" primary={true} />
+                </ToolbarGroup>
+                <ToolbarGroup lastChild>
+                    <FlatButton
+                        onClick={() => { }}
+                        target="_blank"
+                        labelPosition="before"
+                        label="Next"
+                        icon={<ArrowForward />}
+                    />
+                </ToolbarGroup>
+            </Toolbar>
+            <h1>What does the fox say?</h1>
+            <Divider />
+        </div>;
     }
 }
 
