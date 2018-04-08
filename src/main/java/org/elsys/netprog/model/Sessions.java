069ef924b6a311cf2b05260dbfd96c5716e89a04
Hristo Spasov <hristo_sp99@abv.bg>
2018-04-08 13:12:06 +0300

    Add sessions model

---
+++ src/main/java/org/elsys/netprog/model/Sessions.java
@@ -0,0 +1,86 @@
+package org.elsys.netprog.model;
+
+import javax.persistence.Column;
+import javax.persistence.Id;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.io.Serializable;
+import java.sql.Timestamp;
+import java.util.Objects;
+
+@Entity
+@Table(name = "Sessions")
+public class Sessions implements Serializable {
+
+    @Id
+    @Column(columnDefinition = "mysql->int(11)", name = "UserId", nullable = false)
+    private int UserId;
+
+    @Id
+    @Column(columnDefinition = "mysql->int(11)", name = "SessionId", nullable = false)
+    private int SessionId;
+
+    @Column(columnDefinition = "mysql->timestamp", name = "CreatedAt", nullable = false)
+    private Timestamp CreatedAt;
+
+    @Column(columnDefinition = "mysql->timestamp", name = "ExpiresAt", nullable = false)
+    private Timestamp ExpiresAt;
+
+    public Sessions() {}
+
+    public Sessions(int userId, int sessionId, Timestamp createdAt, Timestamp expiresAt) {
+        this.UserId = userId;
+        this.SessionId = sessionId;
+        this.CreatedAt = createdAt;
+        this.ExpiresAt = expiresAt;
+    }
+
+    public int getUserId() {
+        return UserId;
+    }
+
+    public void setUserId(int userId) {
+        UserId = userId;
+    }
+
+    public int getSessionId() {
+        return SessionId;
+    }
+
+    public void setSessionId(int sessionId) {
+        SessionId = sessionId;
+    }
+
+    public Timestamp getCreatedAt() {
+        return CreatedAt;
+    }
+
+    public void setCreatedAt(Timestamp createdAt) {
+        CreatedAt = createdAt;
+    }
+
+    public Timestamp getExpiresAt() {
+        return ExpiresAt;
+    }
+
+    public void setExpiresAt(Timestamp expiresAt) {
+        ExpiresAt = expiresAt;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Sessions sessions = (Sessions) o;
+        return UserId == sessions.UserId &&
+                SessionId == sessions.SessionId &&
+                Objects.equals(CreatedAt, sessions.CreatedAt) &&
+                Objects.equals(ExpiresAt, sessions.ExpiresAt);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(UserId, SessionId, CreatedAt, ExpiresAt);
+    }
+}
